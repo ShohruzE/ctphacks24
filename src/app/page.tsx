a090d3869d9957996a4f5decf59d2e6039ab8e24
NickYaoo <nicklaus.yao@macaulay.cuny.edu>
2024-08-22 16:43:39 -0400

    Thursday Additions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default function LandingPage() {
   return (
-    <div className="min-h-screen">
-      <h1>Home</h1>
+    <div className="flex column justify-center">
+      <div 
+        style={{
+          width: '460px', height: '500px', backgroundColor: 'lightgray', padding: 10, 
+          borderRadius: '10px', marginLeft:'45px', marginRight: '45px', marginBottom: '45px', 
+          marginTop: '45px', display: 'flex', flexDirection: 'column', opacity: '0.8',
+          justifyContent: 'center', alignItems: 'center'
+      }}>
+        <div style={{ marginBottom: '100px' }}>
+          <Button style={{width:'300px', height: '100px', backgroundColor: '#8B7192', opacity:'1.0'}}> Take the Quiz! </Button>
+        </div>
+        <div>
+          <Button style={{width:'300px', height: '100px'}}> Find a Club! </Button>
+        </div>
+      </div>
     </div>
   );
 }
