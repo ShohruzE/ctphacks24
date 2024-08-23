dc7adbaf2465cdca3ec4d19075634a4fe68a93e0
Ynalois Pangilinan <ynaloispangilinan@gmail.com>
2024-08-23 11:55:38 -0400

    club directory working properly, started working on clubProfile page

---
+++ src/app/(root)/clubDirectory/page.tsx
@@ -0,0 +1,10 @@
+import GenerateCLubs from '@/components/GenerateClubs';
+
+export default async function clubDirectory() {
+    return (
+        <div>
+            <h1>Club Directory</h1>
+            <GenerateCLubs />
+        </div>
+    )
+}
