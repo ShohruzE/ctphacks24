fa6a38e449f6d76b41342d554d625cbeefc12e91
Shohruz Ernazarov <shohruzernazarov04@gmail.com>
2024-08-23 20:12:36 -0400

    Complete Club Directory page and modify Navbar

--- src/components/GenerateClubs.tsx
+++ src/components/GenerateClubs.tsx
@@ -3,79 +3,108 @@
 import { db } from "@/firebase";
 import { getDocs, collection } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import Link from "next/link";
+
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import { Globe, Mail, Instagram, Facebook } from "lucide-react";
+
+const mediaIcons: { [key: string]: any } = {
+  website: Globe,
+  email: Mail,
+  instagram: Instagram,
+  facebook: Facebook,
+};
 
 interface ClubProfileProps {
-    club_name: string;
-    club_description: string;
-    club_links: { [key: string]: string };
-    club_type: string;
+  club_name: string;
+  club_description: string;
+  club_links: { [key: string]: string };
+  club_type: string;
 }
 
 export default function GenerateClubs() {
-    const [clubs, setClubs] = useState<ClubProfileProps[]>([]);
+  const [clubs, setClubs] = useState<ClubProfileProps[]>([]);
 
-    const clubRef = collection(db, "clubs");
+  const clubRef = collection(db, "clubs");
 
-    useEffect(() => {
-        const getClubs = async () => {
-            try {
-                const querySnapshot = await getDocs(clubRef);
-                const clubsData: ClubProfileProps[] = [];
-                querySnapshot.forEach((doc) => {
-                    const data = doc.data();
-                    const clubLinks = data.club_links || {};
-                    clubsData.push({
-                        ...data,
-                        club_links: clubLinks,
-                    } as ClubProfileProps);
-                });
+  useEffect(() => {
+    const getClubs = async () => {
+      try {
+        const querySnapshot = await getDocs(clubRef);
+        const clubsData: ClubProfileProps[] = [];
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          const clubLinks = data.club_links || {};
+          clubsData.push({
+            ...data,
+            club_links: clubLinks,
+          } as ClubProfileProps);
+        });
 
-                setClubs(clubsData);
-            } catch (error) {
-                console.error("Error fetching clubs: ", error);
-            }
-        };
-        getClubs();
-    }, []);
+        setClubs(clubsData);
+      } catch (error) {
+        console.error("Error fetching clubs: ", error);
+      }
+    };
+    getClubs();
+  }, []);
 
-    const mediaOrder = ["website", "instagram", "facebook", "linktree", "email"];
+  const clubsByType = clubs.reduce((acc, club) => {
+    if (!acc[club.club_type]) {
+      acc[club.club_type] = [];
+    }
+    acc[club.club_type].push(club);
+    return acc;
+  }, {} as { [key: string]: ClubProfileProps[] });
 
-    const clubsByType = clubs.reduce((acc, club) => {
-        if (!acc[club.club_type]) {
-            acc[club.club_type] = [];
-        }
-        acc[club.club_type].push(club);
-        return acc;
-    }, {} as { [key: string]: ClubProfileProps[] });
-
-    return (
-        <div>
-            <h1>Clubs</h1>
-            {Object.keys(clubsByType).map((type) => (
-                <div key={type}>
-                    <h2>{type}</h2>
-                    <div className="flex flex-row">
-                        {clubsByType[type].map((club, index) => (
-                            <div key={index} className="border-2 w-3/6 rounded-md">
-                                <h1>{club.club_name}</h1>
-                                <p>Club type: {club.club_type}</p>
-                                <p>Description: {club.club_description}</p>
-                                <ul>
-                                    {mediaOrder.map((media) =>
-                                        club.club_links[media] ? (
-                                            <li key={media}>
-                                                <a href={club.club_links[media]} target="_blank">
-                                                    {media.charAt(0).toUpperCase() + media.slice(1)}: {club.club_links[media]}
-                                                </a>
-                                            </li>
-                                        ) : null
-                                    )}
-                                </ul>
-                            </div>
-                        ))}
-                    </div>
-                </div>
+  return (
+    <div className="flex flex-col justify-center items-center">
+      {Object.keys(clubsByType).map((type) => (
+        <div key={type} className="py-16">
+          <div className="flex flex-row flex-wrap gap-8">
+            {clubsByType[type].map((club, index) => (
+              <Card
+                key={index}
+                className="flex flex-col border-2 border-purple-800 w-[350px] rounded-xl"
+              >
+                <CardHeader>
+                  <CardTitle className="font-bold">{club.club_name}</CardTitle>
+                  <h4 className="font-semibold text-slate-500">
+                    {club.club_type}
+                  </h4>
+                </CardHeader>
+                <CardContent className="grow">
+                  <CardDescription>{club.club_description}</CardDescription>
+                </CardContent>
+                <CardFooter className="flex flex-row flex-wrap items-center gap-2">
+                  {Object.keys(mediaIcons).map((media) => {
+                    const Icon = mediaIcons[media];
+                    return (
+                      club.club_links[media] && (
+                        <Link
+                          key={media}
+                          href={club.club_links[media]}
+                          target="_blank"
+                          className="hover:text-purple-800"
+                        >
+                          <Icon size={30} />
+                        </Link>
+                      )
+                    );
+                  })}
+                </CardFooter>
+              </Card>
             ))}
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
